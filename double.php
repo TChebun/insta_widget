cdb07f4ef1cf6f3e9b44dfc4cd2329d30f128d59
TChebun <t.chebun@gmail.com>
2023-05-17 20:25:30 +0200

    add php files and icons

---
+++ double.php
@@ -0,0 +1,56 @@
+<section class="double">
+    <div class="container">
+        <div class="double-title__wrapper">
+            <h2 class="double-title__item">
+                Double your <span>conversions.</span>
+            </h2>
+            <h2 class="double-title__item">
+                Double your <span>growth.</span>
+            </h2>
+        </div>
+        <div class="features">
+            <div class="features__wrapper">
+                <h3 class="features__title">Brands</h3>
+                <h4 class="features__subtitle">
+                    <i class="increase"></i>
+                    Icreases conversions
+                </h4>
+                <p class="features__text">
+                Enhance the visual showcase of your products with unique content making visitors convert to buyers.
+                </p>
+            </div>
+            <div class="features__wrapper">
+                <h3 class="features__title">Marketers</h3>
+                <h4 class="features__subtitle save">
+                    <!-- <i class="save"></i> -->
+                    Save time
+                </h4>
+                <p class="features__text">
+                No more re-purposing, re-cropping or re-uploading the same content from your Instagram stories to your website. 
+                </p>
+            </div>
+            <div class="features__wrapper">
+                <h3 class="features__title">Web designers</h3>
+                <h4 class="features__subtitle">
+                    <i class="designers"></i>
+                    Fresh content
+                </h4>
+                <p class="features__text">
+                Every story you post on Instagram, it is automatically saved in your account. Auto updates you unique content.
+                </p>
+            </div>
+        </div>
+        <div class="author">
+            <img src="assets/image 72.png" alt="Логотип" class="img-fluid author__logo">
+            <p class="author__text">
+            “Storytelling on another level. EmbedStories is great - it takes pride of place on our homepage aggregating stories from our Instagram account. I highly recommend this product. ”.
+            </p>
+            <div class="author__item">
+                <img src="assets/Rectangle 192.jpg" alt="author" class="img-fluid author__item__logo">
+                <div class="author__item__text">
+                   <span>Ben Calahen</span>, COO at Clovin
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php require_once('header.php') ?> 
+<section class="main-screen">
+    <div class="container">
+        <div class="welcome-screen">
+            <div class="welcome">
+                <h1 class="welcome__title">Display Instagram Stories 
+                    on any website,<span> in seconds.</span></h1>
+                <p class="welcome__text">Display Instagram Stories Widget on your website, that auto-syncs, and updates each time you add a photo in your active story. </p>
+                <div class="subscribe">
+                    <input type="text" class="subscribe__input" placeholder="Enter your email...">
+                    <button class="subscribe__button">Start Free Trial</button>
+                </div>
+                <ul class="welcome__list">
+                    <li class="welcome__item">Free 7-day trial</li>
+                    <li class="welcome__item">Easy setup</li>
+                    <li class="welcome__item">Cancel any time</li>
+                </ul>
+            </div>
+            <div class="banner">
+                <img src="assets/main_pic.png" alt="Виджет" class="img-fluid">
+            </div>
+            <div class="partners">
+                <p>Join 50.000+ websites that use
+                    EmbedSocial to sell more</p>
+                <div class="partners__logo">
+                    <a href="/" class="partners__link">
+                        <img src="assets/cnn.svg" alt="Лого">
+                    </a>
+                    <a href="/" class="partners__link">
+                        <img src="assets/netgeo.svg" alt="Лого">
+                    </a>
+                    <a href="/" class="partners__link">
+                        <img src="assets/florencebymills.png" alt="Лого">
+                    </a>
+                    <a href="/" class="partners__link">
+                        <img src="assets/ancestry.png" alt="Лого">
+                    </a>
+                    <a href="/" class="partners__link">
+                        <img src="assets/industrious.png" alt="Лого">
+                    </a>
+                    <a href="/" class="partners__link">
+                        <img src="assets/nike.svg" alt="Лого">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require_once('double.php')?>
+<?php require_once('footer.php') ?> 
